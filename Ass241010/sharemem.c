68cf1bfe36d885e4f60c30d90e57dbf274f39b68
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-10-11 20:49:42 +0700

    gt - mod code

--- Ass241010/sharemem.c
+++ Ass241010/sharemem.c
@@ -8,6 +8,15 @@ Viết chương trình C với yêu cầu sau:
 * Definitions
 ********************************************************************************/
 #include <stdio.h>
+#include <fcntl.h>            //For O_* constants,  shm_open
+#include <unistd.h>         // For ftruncate()
+
+
+#include <sys/types.h>         //pid_t
+#include <unistd.h>             //fork
+
+#define SHM_NAME "my-shm"   //share memory name
+#define SHM_SIZE   (1)              //1*sizeof(smem)
 
 typedef struct t_Share_Memory
 {
@@ -19,9 +28,7 @@ typedef struct t_Share_Memory
 * Prototype
 ********************************************************************************/
 /* Parent proc init 'ready==0'*/
-void init_Memory(smem *mem);
-/* Parent proc get value for x,y*/
-void input_xy(smem *mem);
+int init_ShareMemory(char* name, int size, int*  sid);
 
 
 /********************************************************************************
@@ -29,29 +36,46 @@ void input_xy(smem *mem);
 ********************************************************************************/
 int main()
 {
-    smem shared_mem;
-    //init mem
-    init_Memory(&shared_mem);
+    //variable
+    int shmid;                  //save id of share memory
+    pid_t childPID = -1;   //check parent or child or failed init proc
+    smem*  shmem;
 
-    //get x, y
-    input_xy(&shared_mem);
+    //init sharing mem
+    if(init_ShareMemory(SHM_NAME,SHM_SIZE, &shmid))
+    {
+        return 1;
+    }
 
-    fprintf(stdout,"\nx=[%d],y=[%d]",shared_mem.x,shared_mem.y);
+    //init child
+    childPID = fork();
+
+
+
+
+
 
     // exit ok
     return 0;
 }
 
-void init_Memory(smem *mem)
+int init_ShareMemory(char* name, int size, int*  sid)
 {
-    mem->z=0;
+    int code;
+    *sid = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    //check init mem
+    if(*sid == -1)//error
+    {
+        fprintf(stderr,"[ERR] Failed open share memmory");
+        return 1;
+    }
+    //allocate mem size
+    code = ftruncate(*sid, size*sizeof(smem));
+    if(code == -1)
+    {
+        fprintf(stderr,"[ERR] Failed modify memmory");
+        return 2;
+    }
     //anything ok
-    return;
-}
-
-void input_xy(smem *mem)
-{
-    fprintf(stdout,"\nInput x,y: ");
-    fscanf(stdin,"%d%d",&mem->x,&mem->y);
-    fscanf(stdin,"%*[^\n]%*c");
+    return 0;
 }
