360e0c49526138c62d68990142d0a77746c43e0c
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-10-29 19:21:57 +0700

    g - add SJNlinkedlist lib

--- Ass241024_scheduler/_GiangTrinh/SJN_linkedlist.c
+++ Ass241024_scheduler/_GiangTrinh/SJN_linkedlist.c
@@ -3,6 +3,7 @@
 ********************************************************************************/
 #include "SJN_linkedlist.h"
 #include <stdio.h>
+#include <stdlib.h>//allocate memory
 /********************************************************************************
 * Variables
 ********************************************************************************/
@@ -14,10 +15,29 @@
 /********************************************************************************
 * Code
 ********************************************************************************/
-// Add a new Process to linked list.
-Plist* add_new_to_Plist(Plist* first_proc, uint32_t id, uint32_t arrival_time, uint32_t burst_time)
+// Add a new Process to head of linked list.
+int8_t add_new_to_Plist(Plist** first_proc, uint32_t id, uint32_t arrival_time, uint32_t burst_time)
 {
+    Plist* new_proc = (Plist*)calloc(1, sizeof(Plist));
+    if(new_proc==NULL)
+    {return -1;/*malloc failed*/}
 
+    new_proc->id=id;
+    new_proc->arrival_time=arrival_time;
+    new_proc->burst_time=burst_time;
+
+    if((*first_proc) != NULL)
+    {
+        new_proc->Next = *first_proc;
+        *first_proc = new_proc;
+    }
+    else
+    {
+        *first_proc = new_proc;
+    }
+
+    /* anything ok */
+    return 0;
 }
 
 //Arrange Plist by time arrival.
@@ -76,7 +96,38 @@ int8_t arrange_Plist_by_time_arrival(Plist** first_proc)
 }
 
 //Move a member from this Plist to first of another Plist
-int8_t move_to_first_another_Plist(Plist* first_list1, Plist* memlist1, Plist* first_list2);
+int8_t move_to_first_another_Plist(Plist** first_list1, Plist* memlist1, Plist** first_list2)
+{
+    //cut memlist1
+    if(memlist1 == *first_list1)
+    {
+        *first_list1 = (*first_list1)->Next;
+    }
+    else
+    {
+        Plist* pre_memlist1 = *first_list1;
+        while(pre_memlist1->Next != memlist1)
+        {
+            pre_memlist1=pre_memlist1->Next;
+        }
+        pre_memlist1->Next = memlist1->Next;
+    }
+    //then add it to first of list 2
+    memlist1->Next=*first_list2;
+    *first_list2=memlist1;
+
+    //anything ok
+    return 0;
+}
 
 // Delete the process list.
-int8_t delete_Plist(Plist* first_proc);
+int8_t delete_Plist(Plist** first_proc)
+{
+    while(*first_proc!=NULL)
+    {
+        Plist* temp = *first_proc; 
+        *first_proc = (*first_proc)->Next;
+        free(temp);
+    }
+    return 0;
+}
--- Ass241024_scheduler/_GiangTrinh/SJN_linkedlist.h
+++ Ass241024_scheduler/_GiangTrinh/SJN_linkedlist.h
@@ -22,18 +22,19 @@ typedef struct t_Proc_Linkedlist
 * API
 ********************************************************************************/
 /**
- * @brief Add a new Process to linked list.
+ * @brief Add a new Process to head of linked list.
  * 
- * add a new member to linkedlist.
+ * add a new member to head of linkedlist.
  * Memory for the new member is dynamically allocated.
  * 
+ * @param first_proc is pointer level2 point to pointer of first member in list
  * @param id  Identifier for the process.
  * @param arrival_time  The time at which the process arrives.
  * @param burst_time  The time required to complete the process.
  * 
  * @return int 0 if no error, -1 if an error occurred.
  */
-int8_t add_new_to_Plist(Plist* first_proc, uint32_t id, uint32_t arrival_time, uint32_t burst_time);
+int8_t add_new_to_Plist(Plist** first_proc, uint32_t id, uint32_t arrival_time, uint32_t burst_time);
 
 /**
  * @brief Arrange Plist by time arrival.
@@ -42,21 +43,23 @@ int8_t add_new_to_Plist(Plist* first_proc, uint32_t id, uint32_t arrival_time, u
  * 
  * create a temp list to move member of Plist from lowest to highest time arrival.
  */
-int8_t arrange_Plist_by_time_arrival(Plist* first_proc);
+int8_t arrange_Plist_by_time_arrival(Plist** first_proc);
 
 /**
  * @brief Move a member from this Plist to first of another Plist
+ * 
+ * @return only return 0 :)
  */
-int8_t move_to_first_another_Plist(Plist* first_list1, Plist* memlist1, Plist* first_list2);
+int8_t move_to_first_another_Plist(Plist** first_list1, Plist* memlist1, Plist** first_list2);
 
 /**
  * @brief Delete the process list.
  * This function deallocates the memory used for Process list
  * 
- * @param first_proc Pointer to the first element of the process list.
- * @return int 0 if no error, -1 if an error occurred.
+ * @param first_proc is pointer level2 point to pointer of first member in list
+ * @return only return 0 :)
  */
-int8_t delete_Plist(Plist* first_proc);
+int8_t delete_Plist(Plist** first_proc);
 
 
 #endif/* _SJN_LL_ */
