fe6396565f762ca7fa0284353d1a9d0a13e1a873
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-10-30 08:23:02 +0700

    g - small update

--- Ass241024_scheduler/_GiangTrinh/build.sh
+++ Ass241024_scheduler/_GiangTrinh/build.sh
@@ -4,7 +4,6 @@
 mkdir -p build
 
 #build
-# -pthread: create thread
 gcc -o ./build/scheduler main.c delay_ms.c mapping.c process_handler.c SJN_linkedlist.c -lrt
 
 #export and overwrite to build/output.txt
