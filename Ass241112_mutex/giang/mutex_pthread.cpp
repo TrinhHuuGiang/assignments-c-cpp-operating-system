c5b14bd411efcff066ddff7734f383774fc80da9
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-11-13 18:27:11 +0700

    update ass241112

--- Ass241112_mutex/giang/mutex_pthread.cpp
+++ Ass241112_mutex/giang/mutex_pthread.cpp
@@ -4,9 +4,8 @@
  * Su dung mutex de dong bo hoa hai tien trinh nhu sau:
  * - Process me tao 1 khoa mutex sau do khoa lai
  * - Tao thread con va thread do se bi block
- * - Process me do gia tri x,y tu ban phim sau cho mo khoa va wait thread con
- * - Thread sau do duoc  chay se lay duoc khoa va khoa lai
- * - Sau do thread thuc hien z=x+y
- * - Sau do no nha khoa ra va ket thuc ( :) thuc ra cung cha can khoa vi co wait roi)
+ * - Process me do gia tri x,y tu ban phim sau cho mo khoa va pthread_join thread con
+ * - Thread sau do duoc  chay thuc hien z=x+y
+ * - sau do no ket thuc
  * - sau do process me in ra z,  xoa khoa mutex va thoat chuong trinh
  */
